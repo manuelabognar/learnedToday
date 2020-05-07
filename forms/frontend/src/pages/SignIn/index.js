83d6fbe5d47ab6c86c84c4912304d17922529852
manuelabognar <manuela.camara@usp.br>
2020-05-07 18:58:52 -0300

    structure

---
+++ forms/frontend/src/pages/SignIn/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import { Form } from '@unform/web';
+import Input from '../../components/Input';
+
+export default function SignIn() {
+  function handleSubmit(data) {
+    console.log(data);
+    // { email: 'test@example.com', password: '123456' }
+  }
+
+  return (
+    <div>
+      <h1>Sign In</h1>
+
+      <Form onSubmit={handleSubmit}>
+        <Input name="email" type="email" />
+        <Input name="password" type="password" />
+        <button type="submit">Sign in</button>
+      </Form>
+
+      <Link to="/">Voltar</Link>
+    </div>
+  );
+}
