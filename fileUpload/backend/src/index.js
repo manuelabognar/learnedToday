f75b585017b8776765747fc85d1549583f0dbeba
manuelabognar <manuela.camara@usp.br>
2020-05-13 15:33:25 -0300

    ambiente backend

--- fileUpload/backend/src/index.js
+++ fileUpload/backend/src/index.js
@@ -1,9 +1,12 @@
 const express = require("express");
+const morgan = require("morgan");
 
 const app = express();
 
-app.get("/", (req, res) => {
-  return res.send("Start file upload")
-});
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan('dev'));
+
+app.use(require("./routes"));
 
 app.listen(3333);
