5f000b567dfe34f07b9405a7d8e064d90ed11345
Manuela Bognar <manuelabcamara@gmail.com>
2020-09-13 23:25:54 -0300

    desafio06: lists transactions

--- desafios-rocketseat/desafio06-master/src/repositories/TransactionsRepository.ts
+++ desafios-rocketseat/desafio06-master/src/repositories/TransactionsRepository.ts
@@ -1,6 +1,7 @@
 import { EntityRepository, Repository } from 'typeorm';
 
 import Transaction from '../models/Transaction';
+// import transactionsRouter from '../routes/transactions.routes';
 
 interface Balance {
   income: number;
@@ -10,9 +11,34 @@ interface Balance {
 
 @EntityRepository(Transaction)
 class TransactionsRepository extends Repository<Transaction> {
-  // public async getBalance(): Promise<Balance> {
-  //   // TODO
-  // }
+  public async getBalance(): Promise<Balance> {
+    const transations = await this.find();
+
+    const { income, outcome } = transations.reduce(
+      (accumulator, transation) => {
+        switch (transation.type) {
+          case 'income':
+            accumulator.income += Number(transation.value);
+            break;
+          case 'outcome':
+            accumulator.outcome += Number(transation.value);
+            break;
+          default:
+            break;
+        }
+        return accumulator;
+      },
+      {
+        income: 0,
+        outcome: 0,
+        total: 0,
+      },
+    );
+
+    const total = income - outcome;
+
+    return { income, outcome, total };
+  }
 }
 
 export default TransactionsRepository;
--- desafios-rocketseat/desafio06-master/src/routes/transactions.routes.ts
+++ desafios-rocketseat/desafio06-master/src/routes/transactions.routes.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import { getCustomRepository } from 'typeorm';
 
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import CreateTransactionService from '../services/CreateTransactionService';
@@ -8,7 +9,12 @@ import CreateTransactionService from '../services/CreateTransactionService';
 const transactionsRouter = Router();
 
 transactionsRouter.get('/', async (request, response) => {
-  // TODO
+  const transactionsRepository = getCustomRepository(TransactionsRepository);
+
+  const transactions = await transactionsRepository.find();
+  const balance = await transactionsRepository.getBalance();
+
+  return response.json({ transactions, balance });
 });
 
 transactionsRouter.post('/', async (request, response) => {
@@ -27,7 +33,7 @@ transactionsRouter.post('/', async (request, response) => {
 });
 
 transactionsRouter.delete('/:id', async (request, response) => {
-  // TODO
+
 });
 
 transactionsRouter.post('/import', async (request, response) => {
